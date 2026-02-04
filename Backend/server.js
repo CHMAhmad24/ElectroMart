6f6ca4acf3eaa66baacf60f42735e8d0efa1d008
Ch Muhammad Ahmad <chmahmad24@gmail.com>
2026-02-04 17:46:18 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://electro-mart-lyart.vercel.app",
+  origin: "https://electro-mart-shop.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
