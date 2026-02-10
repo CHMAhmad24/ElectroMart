2b1a4ed2586cda615052f963a73aa7046aa28fa7
Ch Muhammad Ahmad <chmahmad24@gmail.com>
2026-02-10 19:21:46 +0500

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -137,7 +137,7 @@ export const login = async (req, res) => {
                 message: "Email and Password are required"
             })
         }
-        const existingUser = await User.findOne({ email }).select("-password -otp -otpExpiry -_id -token");
+        const existingUser = await User.findOne({ email }).select("-password");
         if (!existingUser) {
             return res.status(400).json({
                 success: false,
