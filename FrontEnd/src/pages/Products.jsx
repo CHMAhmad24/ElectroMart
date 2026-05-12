203dd5a74bb19ec1692f85ab110a3eb35da3c357
Ch Muhammad Ahmad <chmahmad24@gmail.com>
2026-05-12 10:34:12 +0500

    Update Products.jsx

--- FrontEnd/src/pages/Products.jsx
+++ FrontEnd/src/pages/Products.jsx
@@ -99,7 +99,7 @@ const Products = () => {
 
     return (
         <div className='pt-18 pb-5 px-4 sm:px-6 min-h-screen bg-black'>
-            <ParticlesBackground/>
+            
             <div className='max-w-7xl mx-auto flex flex-col lg:flex-row gap-8 relative'>
                 <div className='lg:hidden mb-6'>
                     <div className='relative mt-3 -mb-7'>
